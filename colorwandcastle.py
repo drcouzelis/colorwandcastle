ace8125fcaa35359b72ed524864edd124bfe5b3d
David Couzelis <drcouzelis@gmail.com>
2013-03-16 10:09:03 -0400

    Cleaned up the code. The player now flips correctly.

--- colorwandcastle.py
+++ colorwandcastle.py
@@ -29,10 +29,10 @@ class Bounds:
     def __init__(self, u=0, l=0, d=0, r=0):
         self.u, self.l, self.d, self.r = u, l, d, r
 
-playfield_bounds = Bounds(HEIGHT - Block.size, Block.size, Block.size, WIDTH - Block.size)
-
 class Block(pyglet.sprite.Sprite):
     size = 80
+
+playfield_bounds = Bounds(HEIGHT - Block.size, Block.size, Block.size, WIDTH - Block.size)
 
 class Player(pyglet.window.key.KeyStateHandler):
 
@@ -41,69 +41,92 @@ class Player(pyglet.window.key.KeyStateHandler):
     L = 'left'
 
     def __init__(self):
-        self.sprite = None
         self.dir = Player.R
         self.fly_anm = pyglet.image.Animation((
-            pyglet.image.AnimationFrame(pyglet.resource.image('makayla_fly_1.png'), 1/4),
-            pyglet.image.AnimationFrame(pyglet.resource.image('makayla_fly_2.png'), 1/4)
+            pyglet.image.AnimationFrame(load_img('makayla_fly_1.png'), 1/4),
+            pyglet.image.AnimationFrame(load_img('makayla_fly_2.png'), 1/4)
             ))
         self.stand_anm = pyglet.image.Animation((
-            pyglet.image.AnimationFrame(pyglet.resource.image('makayla_stand_1.png'), 1/4),
-            pyglet.image.AnimationFrame(pyglet.resource.image('makayla_stand_2.png'), 1/4),
-            pyglet.image.AnimationFrame(pyglet.resource.image('makayla_stand_3.png'), 1/4),
-            pyglet.image.AnimationFrame(pyglet.resource.image('makayla_stand_2.png'), 1/4)
+            pyglet.image.AnimationFrame(load_img('makayla_stand_1.png'), 1/4),
+            pyglet.image.AnimationFrame(load_img('makayla_stand_2.png'), 1/4),
+            pyglet.image.AnimationFrame(load_img('makayla_stand_3.png'), 1/4),
+            pyglet.image.AnimationFrame(load_img('makayla_stand_2.png'), 1/4)
             ))
         self.walk_anm = pyglet.image.Animation((
-            pyglet.image.AnimationFrame(pyglet.resource.image('makayla_walk_1.png'), 1/4),
-            pyglet.image.AnimationFrame(pyglet.resource.image('makayla_walk_2.png'), 1/4),
-            pyglet.image.AnimationFrame(pyglet.resource.image('makayla_walk_3.png'), 1/4),
+            pyglet.image.AnimationFrame(load_img('makayla_walk_1.png'), 1/6),
+            pyglet.image.AnimationFrame(load_img('makayla_walk_2.png'), 1/6),
+            pyglet.image.AnimationFrame(load_img('makayla_walk_3.png'), 1/6),
             ))
-        self.bounds = Bounds(171, 52, 51, 112)
+        self.bounds = Bounds(60, 30, 60, 30)
+        self.sprite = pyglet.sprite.Sprite(self.fly_anm)
         self.to_flying()
 
     @property
     def x(self):
-        return self.sprite.x if self.sprite else 0
-
-    @property
-    def y(self):
-        return self.sprite.y if self.sprite else 0
+        return self.sprite.x
 
     @x.setter
     def x(self, value):
         self.sprite.x = value
 
+    @property
+    def y(self):
+        return self.sprite.y
+
     @y.setter
     def y(self, value):
         self.sprite.y = value
 
+    @property
+    def u(self):
+        return self.sprite.y + self.bounds.u
+
+    @u.setter
+    def u(self, value):
+        self.sprite.y = value - self.bounds.u
+
+    @property
+    def d(self):
+        return self.sprite.y - self.bounds.d
+
+    @d.setter
+    def d(self, value):
+        self.sprite.y = value + self.bounds.d
+
+    @property
+    def l(self):
+        return self.sprite.x - self.bounds.l
+
+    @l.setter
+    def l(self, value):
+        self.sprite.x = value + self.bounds.l
+
+    @property
+    def r(self):
+        return self.sprite.x + self.bounds.r
+
+    @r.setter
+    def r(self, value):
+        self.sprite.x = value - self.bounds.r
+
     def to_flying(self):
-        if self.sprite:
-            self.sprite.delete()
+        self.sprite.delete()
         flip = True if self.dir == Player.L else False
         anm = self.fly_anm.get_transform(flip_x=flip)
-        if flip:
-            anm.anchor_x = 0
         self.sprite = pyglet.sprite.Sprite(anm, x=self.x, y=self.y, batch=batch, group=character_group)
         self.update = self.update_flying
 
     def to_standing(self):
-        if self.sprite:
-            self.sprite.delete()
+        self.sprite.delete()
         flip = True if self.dir == Player.L else False
         anm = self.stand_anm.get_transform(flip_x=flip)
-        if flip:
-            anm.anchor_x = 0
         self.sprite = pyglet.sprite.Sprite(anm, x=self.x, y=self.y, batch=batch, group=character_group)
         self.update = self.update_standing
 
     def to_walking(self):
-        if self.sprite:
-            self.sprite.delete()
+        self.sprite.delete()
         flip = True if self.dir == Player.L else False
         anm = self.walk_anm.get_transform(flip_x=flip)
-        if flip:
-            anm.anchor_x = 0
         self.sprite = pyglet.sprite.Sprite(anm, x=self.x, y=self.y, batch=batch, group=character_group)
         self.update = self.update_walking
 
@@ -159,17 +182,17 @@ class Player(pyglet.window.key.KeyStateHandler):
             self.to_standing()
 
     def on_floor(self):
-        return self.y + self.bounds.d == playfield_bounds.d
+        return self.d == playfield_bounds.d
 
     def bound(self, bounds):
-        if self.y + self.bounds.d < bounds.d:
-            self.y = bounds.d - self.bounds.d
-        if self.x + self.bounds.l < bounds.l:
-            self.x = bounds.l - self.bounds.l
-        if self.y + self.bounds.u > bounds.u:
-            self.y = bounds.u - self.bounds.u
-        if self.x + self.bounds.r > bounds.r:
-            self.x = bounds.r - self.bounds.r
+        if self.d < bounds.d:
+            self.d = bounds.d
+        if self.l < bounds.l:
+            self.l = bounds.l
+        if self.u > bounds.u:
+            self.u = bounds.u
+        if self.r > bounds.r:
+            self.r = bounds.r
 
 # Create the window
 window = pyglet.window.Window(width=WIDTH, height=HEIGHT, caption='Colorwand Castle')
