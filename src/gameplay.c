a365b5fc743fc1aba14e6ce1445b72ca4dc2182e
David Couzelis <drcouzelis@gmail.com>
2015-03-19 21:24:34 -0400

    Combined files.

--- src/gameplay.c
+++ src/gameplay.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 
 #include "anim.h"
-#include "hero.h"
 #include "main.h"
 #include "utilities.h"
 
@@ -14,8 +13,6 @@ static int end_gameplay = 0;
 #define HERO_SPEED ((TILE_SIZE) * 4)
 #define PPS_TO_TICKS(PPS) ((PPS) / (float)(GAME_TICKER))
 
-
-static HERO hero;
 
 static int board[ROWS][COLS];
 
@@ -34,6 +31,166 @@ typedef enum
     BOARD_PURPLE,
     BOARD_LAST_COLOR
 } BOARD_TYPES;
+
+
+typedef enum
+{
+    STAR_FIRST_COLOR = 0,
+    STAR_RED = 0,
+    STAR_ORANGE,
+    STAR_YELLOW,
+    STAR_GREEN,
+    STAR_BLUE,
+    STAR_PURPLE,
+    STAR_LAST_COLOR,
+} STAR_COLOR;
+
+
+typedef struct
+{
+    ANIM anim;
+    int color;
+
+    float x;
+    float y;
+
+    int is_moving;
+    int is_forward;
+    int is_exploding;
+} STAR;
+
+
+int init_star(STAR *star, int color)
+{
+    IMAGE *image;
+
+    /* Init anim based on color */
+    switch (color) {
+        case STAR_RED:
+            image = IMG("star-red.png");
+            break;
+        case STAR_ORANGE:
+            image = IMG("star-orange.png");
+            break;
+        case STAR_YELLOW:
+            image = IMG("star-yellow.png");
+            break;
+        case STAR_GREEN:
+            image = IMG("star-green.png");
+            break;
+        case STAR_BLUE:
+            image = IMG("star-blue.png");
+            break;
+        case STAR_PURPLE:
+            image = IMG("star-purple.png");
+            break;
+        default:
+            image = NULL;
+    }
+
+    init_anim(&star->anim, 1, 2);
+    add_frame(&star->anim, image);
+    add_frame(&star->anim, image);
+
+    star->color = color;
+
+    /* Init position based on hero */
+    star->x = 0;
+    star->y = 0;
+
+    star->is_moving = 0;
+    star->is_forward = 1;
+    star->is_exploding = 0;
+
+    return 1;
+}
+
+
+typedef struct
+{
+    ANIM anim;
+    float x;
+    float y;
+
+    /* Movement toggles */
+    /* True means the hero is moving in that direction */
+    int u;
+    int d;
+    int l;
+    int r;
+
+    STAR star;
+} HERO;
+
+
+static HERO hero;
+
+
+int init_hero(HERO *hero, float x, float y)
+{
+    init_anim(&hero->anim, 1, 10);
+    add_frame(&hero->anim, IMG("makayla-01.png"));
+    add_frame(&hero->anim, IMG("makayla-02.png"));
+
+    /* Set the starting position */
+    hero->x = x;
+    hero->y = y;
+
+    hero->u = 0;
+    hero->d = 0;
+    hero->l = 0;
+    hero->r = 0;
+
+    init_star(&hero->star, STAR_RED);
+
+    return 1;
+}
+
+
+void control_hero(HERO *hero, ALLEGRO_EVENT *event)
+{
+    int key = 0;
+
+    if (event->type == ALLEGRO_EVENT_KEY_DOWN) {
+        key = event->keyboard.keycode;
+
+        /* Hero key pressed */
+        switch (key) {
+            case ALLEGRO_KEY_UP:
+                hero->u = 1;
+                break;
+            case ALLEGRO_KEY_DOWN:
+                hero->d = 1;
+                break;
+            case ALLEGRO_KEY_LEFT:
+                hero->l = 1;
+                break;
+            case ALLEGRO_KEY_RIGHT:
+                hero->r = 1;
+                break;
+        }
+    }
+
+    if (event->type == ALLEGRO_EVENT_KEY_UP) {
+        key = event->keyboard.keycode;
+
+        /* Hero key released */
+        switch (key) {
+            case ALLEGRO_KEY_UP:
+                hero->u = 0;
+                break;
+            case ALLEGRO_KEY_DOWN:
+                hero->d = 0;
+                break;
+            case ALLEGRO_KEY_LEFT:
+                hero->l = 0;
+                break;
+            case ALLEGRO_KEY_RIGHT:
+                hero->r = 0;
+                break;
+        }
+    }
+}
 
 
 void setup_board(int num_cols, int num_colors)
