b5133f1400c71f7613916de89206a3511b563e43
David Couzelis <drcouzelis@gmail.com>
2016-04-11 21:59:12 -0400

    Code cleanup.

--- src/main.h
+++ src/main.h
@@ -2,23 +2,8 @@
 #define MAIN_HEADER
 
 
-/*#include <allegro5/allegro.h>*/
-
-
 #define GAME_TICKER 100
 
-
-/* Colors */
-/*
-#define WHITE (al_map_rgb(255, 255, 255))
-#define BLACK (al_map_rgb(0, 0, 0))
-#define GREEN (al_map_rgb(0, 255, 0))
-#define BLUE (al_map_rgb(0, 0, 100))
-#define LIGHT_BLUE (al_map_rgb(130, 229, 255))
-#define RED (al_map_rgb(109, 4, 4))
-#define GRAY (al_map_rgb(109, 109, 109))
-#define MAGICPINK (al_map_rgb(255, 0, 255))
-*/
 
 #define TILE_SIZE 20
 
