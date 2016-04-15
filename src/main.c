49a9d02ecbbace8bce34facf35d182fc6795276d
David Couzelis <drcouzelis@gmail.com>
2016-04-15 19:03:14 -0400

    Converted main to SDL.

--- src/main.c
+++ src/main.c
@@ -1,30 +1,35 @@
-#include <allegro5/allegro.h>
+#include <SDL2/SDL.h>
 #include <stdio.h>
-
 #include "config.h"
-
 #include "gameplay.h"
 #include "main.h"
 #include "memory.h"
+#include "resources.h"
 #include "run.h"
 #include "sprite.h"
-#include "resources.h"
 
+#define CWC_TILE_SIZE 20 
+ 
+#define CWC_COLS 16 
+#define CWC_ROWS 12 
+ 
+#define CWC_DISPLAY_WIDTH (CWC_COLS * CWC_TILE_SIZE) 
+#define CWC_DISPLAY_HEIGHT (CWC_ROWS * CWC_TILE_SIZE) 
+
+SDL_Window* GAME_Display = NULL;
 
 int main(int argc, char **argv)
 {
-    ALLEGRO_DISPLAY *display = NULL;
-  
     /*
     int monitor_w = DISPLAY_WIDTH;
     int monitor_h = DISPLAY_HEIGHT;
     int scale = 1;
     */
 
-    SCENE *scene = NULL;
+    /*SCENE *scene = NULL;*/
   
-    if (!al_init() || !al_init_image_addon() || !al_install_keyboard() || !al_install_mouse()) {
-        fprintf(stderr, "Failed to initialize allegro.\n");
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        fprintf(stderr, "SDL_Init failed: %s\n", SDL_GetError());
         exit(1);
     }
   
@@ -35,25 +40,25 @@ int main(int argc, char **argv)
     */
   
     /*display = al_create_display(DISPLAY_WIDTH * scale, DISPLAY_HEIGHT * scale);*/
-    display = al_create_display(DISPLAY_WIDTH, DISPLAY_HEIGHT);
-    if (!display) {
-        fprintf(stderr, "Failed to create display.\n");
+    GAME_Display = SDL_CreateWindow("Colorwand Castle", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, DISPLAY_WIDTH, DISPLAY_HEIGHT, SDL_WINDOW_SHOWN);
+
+    if (!GAME_Display) {
+        fprintf(stderr, "SDL_CreateWindow failed: %s\n", SDL_GetError());
+        SDL_Quit();
         exit(1);
     }
   
-    al_set_target_bitmap(al_get_backbuffer(display));
     /*set_display_scaling(scale);*/
   
     /* Hide the mouse cursor */
-    al_hide_mouse_cursor(display);
+    /*al_hide_mouse_cursor(GAME_Display);*/
   
     set_animation_fps(GAME_TICKER);
     add_resource_path( PKGDATADIR "/images/");
     add_resource_path( PKGDATADIR "/sounds/");
   
     /* Set the window title and icon */
-    al_set_window_title(display, "Colorwand Castle");
-    al_set_display_icon(display, IMG("icon.png"));
+    /*al_set_display_icon(GAME_Display, IMG("icon.png"));*/
 
     /* Turn audio off */
     toggle_audio();
@@ -61,16 +66,20 @@ int main(int argc, char **argv)
     set_fps(GAME_TICKER);
 
     /* START THE GAME */
+    /*
     scene = create_scene_01();
     run(control_scene, update_scene, draw_scene, scene);
     destroy_scene(scene);
+    */
+    SDL_FillRect(SDL_GetWindowSurface(GAME_Display), NULL, SDL_MapRGB(SDL_GetWindowSurface(GAME_Display)->format, 0xFF, 0x00, 0xFF));
+    SDL_UpdateWindowSurface(GAME_Display);
+    SDL_Delay(2000);
   
     /* DONE, clean up */
     stop_resources();
-    al_destroy_display(display);
+    SDL_DestroyWindow(GAME_Display);
 
     check_memory();
     
     return EXIT_SUCCESS;
 }
-
