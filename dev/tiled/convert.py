98ed0ab2f17ac9f73960d4ea6c2df37d7da8021f
David Couzelis <drcouzelis@gmail.com>
2017-07-23 21:23:13 -0400

    Update tile and template data.

--- dev/tiled/convert.py
+++ dev/tiled/convert.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python3
+
+import sys
 import xmltodict
-with open('room-level-001.tmx') as f:
+
+def split_map(m):
+    for n in m.split('\n'):
+        first = True
+        for c in n.split(','):
+            if c:
+                if not first:
+                    print(' ', end='')
+                else:
+                    first = False
+                print('{:03}'.format(int(c)), end='')
+        print()
+
+with open(sys.argv[1]) as f:
     map = xmltodict.parse(f.read())
+
 print(map['map']['layer'][0]['@name'])
-print(map['map']['layer'][0]['data']['#text'])
+split_map(map['map']['layer'][0]['data']['#text'])
+print()
+
+print(map['map']['layer'][1]['@name'])
+split_map(map['map']['layer'][1]['data']['#text'])
